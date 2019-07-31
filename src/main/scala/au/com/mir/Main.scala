678079d98e957a268811c88969e351962851e45c
mirawlin <31852724+mirawlin@users.noreply.github.com>
2019-07-31 16:59:49 +1000

    added main to run read csv and write to parquet (#2)

---
+++ src/main/scala/au/com/mir/Main.scala
@@ -0,0 +1,32 @@
+package au.com.mir
+
+import au.com.mir.io.Io
+import org.apache.spark.sql.SparkSession
+
+object Main extends App {
+  implicit val session: SparkSession = SparkSupport.getOrCreateSession()
+
+  val df = Io.readCsv("src/main/resources/degrees-that-pay-back.csv")
+
+  Io.writeParquet(df, "target/degrees-that-pay-back")
+
+}
+
+object SparkSupport {
+
+  def createSession(appName: String = getClass.getName): SparkSession = {
+    SparkSession.clearActiveSession()
+    val globalSession = SparkSession.getDefaultSession.getOrElse(SparkSession.builder()
+      .appName("Global session")
+      .master("local[2]")
+      .getOrCreate())
+    val session = globalSession.newSession()
+    session.conf.set("spark.app.name", appName)
+    SparkSession.setActiveSession(session)
+    session
+  }
+
+  def getOrCreateSession(appName: String = getClass.getName): SparkSession = SparkSession
+    .getActiveSession
+    .getOrElse(createSession(appName))
+}
